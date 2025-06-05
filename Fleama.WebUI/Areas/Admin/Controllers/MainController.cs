24a9a191459be9e70e376888fecbb10fc76ac3a7
irem doğan <iremydgn@gmail.com>
2025-06-05 21:30:00 +0300

    admin ekranı

--- Fleama.WebUI/Areas/Admin/Controllers/MainController.cs
+++ Fleama.WebUI/Areas/Admin/Controllers/MainController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fleama.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fleama.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class MainController : Controller
     {
-        public IActionResult Index()
+        private readonly DatabaseContext _context;
+
+        public MainController(DatabaseContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.TotalUsers = await _context.AppUsers.CountAsync();
+            ViewBag.TotalCategories = await _context.Categories.CountAsync();
+            ViewBag.TotalNews = await _context.News.CountAsync();
+
             return View();
         }
     }
